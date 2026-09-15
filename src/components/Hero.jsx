34ab8af251c345492b75742dd828fa344f416411
jasim asharaf <jazimashrf123@gmail.com>
2026-09-15 23:48:57 +0530

    Update Hero founders image to official Cloudinary URL

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ export default function Hero({ onOpenContact }) {
           <div className="founder-image-card glass-card">
             <div className="card-image-wrapper">
               <img 
-                src="/assets/founders.jpg" 
+                src="https://res.cloudinary.com/zxszrucp/image/upload/v1789496151/WhatsApp_Image_2026-09-15_at_12.18.55_AM.jpg" 
                 alt="Co-founders of Team RusticHands and RH Growth.in" 
                 className="founder-img"
               />
